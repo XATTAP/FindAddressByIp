2f349129c5662d58bd4ea090c65b55a62214b680
XATTAP <cherednikovkirill@gmail.com>
2024-10-04 13:56:29 +0300

    add tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -15,8 +16,37 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('watch ip 218.108.149.37', () => {
+      const req = { ip: '218.108.149.37' };
+      const resp = {
+        lat: 30.2994,
+        lng: 120.1612,
+        country: 'CN',
+        city: 'Hangzhou',
+      };
+
+      expect(appController.getIp(req)).toEqual(resp);
+    });
+    it('watch ip 192.168.1.3', () => {
+      const req = { ip: '192.168.1.3' };
+
+      expect(() => {
+        appController.getIp(req);
+      }).toThrow(new NotFoundException({ message: 'Ip not found' }));
+    });
+    it('watch ip foo', () => {
+      const req = { ip: 'foo' };
+
+      expect(() => {
+        appController.getIp(req);
+      }).toThrow(new NotFoundException({ message: 'Ip not found' }));
+    });
+    it('watch ip ""', () => {
+      const req = { ip: '' };
+
+      expect(() => {
+        appController.getIp(req);
+      }).toThrow(new NotFoundException({ message: 'Ip not found' }));
     });
   });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,89 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import {
+  BadRequestException,
+  INestApplication,
+  ValidationError,
+  ValidationPipe,
+  ValidationPipeOptions,
+} from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
+    const globalPipesOptions: ValidationPipeOptions = {
+      whitelist: true,
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        const formattedErrors = validationErrors.map((error) => ({
+          [error.property]: error.constraints,
+        }));
+        return new BadRequestException(formattedErrors);
+      },
+    };
+
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe(globalPipesOptions));
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('(GET) /?ip=218.108.149.37', () => {
+    const req = { ip: '218.108.149.37' };
+    const resp = {
+      lat: 30.2994,
+      lng: 120.1612,
+      country: 'CN',
+      city: 'Hangzhou',
+    };
+
+    return request(app.getHttpServer()).get('/').query(req).expect(200, resp);
+  });
+  it('(GET) /?ip=192.168.1.3', () => {
+    const req = { ip: '192.168.1.3' };
+    const resp = { message: 'Ip not found' };
+
     return request(app.getHttpServer())
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .query(req)
+      .expect(404)
+      .expect(({ body }) => {
+        expect(body).toEqual(resp);
+      });
+  });
+  it('(GET) /?ip=asdf', () => {
+    const req = { ip: 'asdf' };
+    const resp = [{ ip: { isIp: 'ip must be an ip address' } }];
+
+    return request(app.getHttpServer())
+      .get('/')
+      .query(req)
+      .expect(400)
+      .expect(({ body }) => {
+        expect(body.message).toEqual(resp);
+      });
+  });
+  it('(GET) /?ip=', () => {
+    const req = { ip: '' };
+    const resp = [
+      {
+        ip: {
+          isIp: 'ip must be an ip address',
+          isNotEmpty: 'ip should not be empty',
+        },
+      },
+    ];
+
+    return request(app.getHttpServer())
+      .get('/')
+      .query(req)
+      .expect(400)
+      .expect(({ body }) => {
+        expect(body.message).toEqual(resp);
+      });
   });
 });
